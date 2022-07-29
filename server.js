e9fc80e7967f47c49193de3c63594d36f4d45eaa
Nicholas <nikolaj.belyakov.84@mail.ru>
2022-07-29 23:13:42 +0300

    get and post routing

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const dotenv = require('dotenv');
+
+dotenv.config({path: '.env-local'});
+
+const PORT = process.env.PORT || '3000';
+
+const app = express();
+
+/**
+* Middleware
+*/
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+/**
+* Routes
+*/
+app.get('/',(request, response) => {
+	response.status(200).send("This is not why you are here. Head to /user/:id and replace the id with your user id.");
+});
+
+const userRouter = require('./routes/user');
+app.use('/user/', userRouter);
+
+app.listen(PORT, () => {
+	console.log(`Listening for requests on port ${PORT}`);
+});
+
